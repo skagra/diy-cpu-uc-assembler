35205c539e5168d2ddb50a55886c6c07de14b343
Paul Harvey <Paul Harvey>
2023-03-27 08:54:49 +0100

    Code copied over from DIY-CPU project

---
+++ microasm/MicroAsmConstants.cs
@@ -0,0 +1,8 @@
+namespace MicroAsm
+{
+    public static class MicroAsmConstants
+    {
+        // Characters which introduce a comment in the uCode file
+        public const string COMMENT_CHARACTERS = "//";
+    }
+}
